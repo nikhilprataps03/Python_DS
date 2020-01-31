6943410e73a075d2fa57b3c083998987928f9dce
Nikhil Pratap Singh <60510072+nikhilprataps03@users.noreply.github.com>
2020-02-01 00:26:53 +0530

    Add files via upload

---
+++ FirstDay.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+age=24
+year=2020
+print('Hello! Nikhil',age)
+print('Happy New Year',year)
+
+
+# In[7]:
+
+
+age=24
+print('your age is {}'.format(age))
+
+
+# In[24]:
+
+
+'''
+You are indian citizen living in noida and your age is 24
+using .format() method
+'''
+
+
+# In[9]:
+
+
+age=24
+Citizen='Indian'
+Location='Noida'
+print('You are {} citizen living in {} and your age is {}'.format(Citizen,Location,age))
+
+
+# In[10]:
+
+
+print('{} and {} = {}'.format(24,23,24+23))
+
+
+# In[19]:
+
+
+age=input('Enter your age')
+
+
+# In[23]:
+
+
+print(type(age))
+# to find the type of the class
+
+
+# In[21]:
+
+
+len(age)
+#To find the of the string
+
+
+# In[22]:
+
+
+print('Nikhil age is',age)
+#Concatenation method
+
+
+# In[ ]:
+
+
+
+
